0e2b3cb8fc2fb71aaf5a6ed9825b5d91516bef94
eyyytran <ltatran94@gmail.com>
2022-07-31 18:46:28 -0400

    styled the index
    i need to add a hamburger icon later

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 //TODO: When we arrive on the home page, if the user is logged in, display the user name in the header, do not display log-in/sign-up button
-const loginBtn = document.getElementById('login-button')
-const dashboardBtn = document.getElementById('see-dashboard')
-const signupBtn = document.getElementById('sign-up-button')
+const loginBtn = document.getElementById('loginbtn')
+const dashboardBtn = document.getElementById('dashboardbtn')
+const signupBtn = document.getElementById('signupbtn')
 
 loginBtn.onclick = () => (window.location.href = 'http://localhost:3001/login')
 dashboardBtn.onclick = () =>
