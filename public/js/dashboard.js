c4be712f282edd499a46dbe70f225ba52ff78817
eyyytran <ltatran94@gmail.com>
2022-08-01 18:02:39 -0400

    changes to dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,11 @@
 const createbtn = document.getElementById('d-createbtn')
-const card = document.getElementById('d-project-card')
+const card = document.getElementsByClassName('d-project-card')
 
 card.addEventListener('click', () => console.log('I got clicked too'))
 
 createbtn.addEventListener('click', () => console.log('I got clicked'))
 
-window.addEventListener('DOMContentLoaded', console.log('the page has loaded'))
+window.addEventListener('DOMContentLoaded', () => loadProjects())
 
 // const loadProjects = async () => {
 //     const projects = await fetch('http://localhost:3001/projects/get_projects')
