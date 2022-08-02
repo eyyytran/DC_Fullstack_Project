55f6f7733b512d9e3ddef32f3677ff872e07320f
Jason <jason.blunck@gmail.com>
2022-08-01 20:21:34 -0500

    reverted eventlistener

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,9 +5,4 @@ card.addEventListener('click', () => console.log('I got clicked too'))
 
 createbtn.addEventListener('click', () => console.log('I got clicked'))
 
-window.addEventListener('DOMContentLoaded', console.log('the page has loaded'))
-
-// const loadProjects = async () => {
-//     const projects = await fetch('http://localhost:3001/projects/get_projects')
-
-// }
+window.addEventListener("DOMContentLoaded", console.log("the page has loaded"));
