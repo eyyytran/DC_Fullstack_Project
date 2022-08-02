698e083bd2a75556006aa74abbc75ed373a9e49b
eyyytran <ltatran94@gmail.com>
2022-08-02 18:35:54 -0400

    rendered task cards to page

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-//TODO: When we arrive on the home page, if the user is logged in, display the user name in the header, do not display log-in/sign-up button
 const loginBtn = document.getElementById('h-loginbtn')
 const dashboardBtn = document.getElementById('h-dashboardbtn')
 const signupBtn = document.getElementById('h-signupbtn')
--- public/js/project.js
+++ public/js/project.js
@@ -1,4 +1,8 @@
-console.log('hello world')
+const signoutbtn = document.querySelector('#p-logoutbtn')
+const toDoList = document.getElementById('todo')
+const doingList = document.getElementById('inprogress')
+const reviewList = document.getElementById('review')
+const completeList = document.getElementById('complete')
 
 const loadCards = async () => {
     const projectID = localStorage.getItem('projectId')
@@ -12,6 +16,7 @@ const loadCards = async () => {
             body: JSON.stringify(requestData),
         })
         const cards = await sendData.json()
+        console.log(typeof cards)
         generateCards(cards)
     } catch (error) {
         alert('could not load your project')
@@ -19,36 +24,46 @@ const loadCards = async () => {
 }
 
 const generateCards = list => {
-    for (let card = 0; card < list.length; card++) {
-        const toDoList = document.getElementById('p-todo-container')
-        const doingList = document.getElementById('p-doing-container')
-        const reviewList = document.getElementById('p-review-container')
-        const completeList = document.getElementById('p-complete-container')
+    for (let i = 0; i < list.length; i++) {
+        const status = list[i].status.toLowerCase()
+        const card = document.createElement('div')
+        const editbtn = document.createElement('button')
+        const editbtnimage = document.createElement('img')
+        const content = list[i].name
+        const cardId = list[i].id.toString()
+        editbtnimage.src =
+            'https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png'
+        card.innerHTML = content
 
-        const cardContainer = document.createElement('div')
+        editbtn.classList.add('p-editbtn')
+        editbtnimage.classList.add('p-editbtn-image')
+        card.classList.add(`${status}`)
+        card.setAttribute('id', cardId)
+        if (status === 'todo') {
+            toDoList.append(card)
+            card.append(editbtn)
+            editbtn.append(editbtnimage)
+        } else if (status === 'inprogress') {
+            doingList.append(card)
+            card.append(editbtn)
+            editbtn.append(editbtnimage)
+        } else if (status === 'review') {
+            reviewList.append(card)
+            card.append(editbtn)
+            editbtn.append(editbtnimage)
+        } else {
+            completeList.append(card)
+            card.append(editbtn)
+            editbtn.append(editbtnimage)
+        }
 
-        let status = list[card].status
-
-        // const cardContainer = document.createElement('div')
-        // const card = document.createElement('div')
-        // const editbtn = document.createElement('button')
-        // const editbtnimage = document.createElement('img')
-        // const content = list[card].name
-        // const projectId = list[card].id
-        // editbtnimage.src =
-        //     'https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png'
-        // card.innerHTML = content
-        // cardContainer.classList.add(`p-${status}-container`)
-        // editbtn.classList.add('p-editbtn')
-        // editbtnimage.classList.add('p-editbtn-image')
-        // card.classList.add('p-project-card')
-        // card.setAttribute('id', projectId)
-
-        // projectList.append(card)
-        // card.append(editbtn)
-        // editbtn.append(editbtnimage)
+        console.log('I made it')
     }
 }
+
+signoutbtn.addEventListener('click', () => {
+    signOutUser()
+})
 
 window.addEventListener('DOMContentLoaded', () => {
     loadCards()
