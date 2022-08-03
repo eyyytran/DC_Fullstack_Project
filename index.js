44ae4b827ca03eafb15e0e1ef9b77fe424099474
eyyytran <ltatran94@gmail.com>
2022-08-03 09:48:47 -0400

    fixed sign out bug

--- index.js
+++ index.js
@@ -1,123 +1,135 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const es6Renderer = require("express-es6-template-engine");
-const models = require("./db/models");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const es6Renderer = require('express-es6-template-engine')
+const models = require('./db/models')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const store = new SequelizeStore({
-  db: models.sequelize,
-});
-const PORT = 3001;
+    db: models.sequelize,
+})
+const PORT = 3001
 // import routes
-const usersRoutes = require("./routes/users");
-const projectsRoutes = require("./routes/projects");
-const cardsRoutes = require("./routes/cards");
-const { getTitle, getScript } = require("./util/locals");
+const usersRoutes = require('./routes/users')
+const projectsRoutes = require('./routes/projects')
+const cardsRoutes = require('./routes/cards')
+const { getTitle, getScript } = require('./util/locals')
 //middleware
 app.use(
-  cors({ origin: "http://127.0.0.1:5500", methods: "GET,POST,PUT,DELETE" })
-);
-app.use(express.json());
-app.use("/public", express.static("./public"));
-app.engine("html", es6Renderer);
-app.set("views", "./views");
-app.set("view engine", "html");
-app.use(cookieParser());
+    cors({ origin: 'http://127.0.0.1:5500', methods: 'GET,POST,PUT,DELETE' })
+)
+app.use(express.json())
+app.use('/public', express.static('./public'))
+app.engine('html', es6Renderer)
+app.set('views', './views')
+app.set('view engine', 'html')
+app.use(cookieParser())
 app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-    store: store,
-  })
-);
-store.sync();
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        store: store,
+    })
+)
+store.sync()
 // validate user
 const checkLogin = (req, res, next) => {
-  console.log("check", req.session.user);
-  if (req.session.user) {
-    next();
-  } else {
-    res.render("template", {
-      locals: {
-        title: getTitle("index"),
-        script: getScript("index"),
-      },
-      partials: {
-        partial: "index",
-      },
-    });
-  }
-};
+    console.log('check', req.session.user)
+    if (req.session.user) {
+        next()
+    } else {
+        res.render('template', {
+            locals: {
+                title: getTitle('index'),
+                script: getScript('index'),
+            },
+            partials: {
+                partial: 'index',
+            },
+        })
+    }
+}
 
 // use routes
-app.use("/users", usersRoutes);
-app.use("/projects", checkLogin, projectsRoutes);
-app.use("/cards", checkLogin, cardsRoutes);
+app.use('/users', usersRoutes)
+app.use('/projects', checkLogin, projectsRoutes)
+app.use('/cards', checkLogin, cardsRoutes)
 
-app.get("/index", (req, res) => {
-  res.render("template", {
-    locals: {
-      title: getTitle("index"),
-      script: getScript("index"),
-    },
-    partials: {
-      partial: "index",
-    },
-  });
-});
+app.get('/', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: getTitle('index'),
+            script: getScript('index'),
+        },
+        partials: {
+            partial: 'index',
+        },
+    })
+})
 
-app.get("/login", (req, res) => {
-  res.render("template", {
-    locals: {
-      title: getTitle("login"),
-      script: getScript("login"),
-    },
-    partials: {
-      partial: "login",
-    },
-  });
-});
+app.get('/index', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: getTitle('index'),
+            script: getScript('index'),
+        },
+        partials: {
+            partial: 'index',
+        },
+    })
+})
 
-app.get("/dashboard", checkLogin, (req, res) => {
-  res.render("template", {
-    locals: {
-      title: getTitle("dashboard"),
-      script: getScript("dashboard"),
-    },
-    partials: {
-      partial: "dashboard",
-    },
-  });
-});
+app.get('/login', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: getTitle('login'),
+            script: getScript('login'),
+        },
+        partials: {
+            partial: 'login',
+        },
+    })
+})
 
-app.get("/project", checkLogin, (req, res) => {
-  res.render("template", {
-    locals: {
-      title: getTitle("project"),
-      script: getScript("project"),
-    },
-    partials: {
-      partial: "project",
-    },
-  });
-});
+app.get('/dashboard', checkLogin, (req, res) => {
+    res.render('template', {
+        locals: {
+            title: getTitle('dashboard'),
+            script: getScript('dashboard'),
+        },
+        partials: {
+            partial: 'dashboard',
+        },
+    })
+})
 
-app.get("/signup", (req, res) => {
-  res.render("template", {
-    locals: {
-      title: getTitle("signup"),
-      script: getScript("signup"),
-    },
-    partials: {
-      partial: "signup",
-    },
-  });
-});
+app.get('/project', checkLogin, (req, res) => {
+    res.render('template', {
+        locals: {
+            title: getTitle('project'),
+            script: getScript('project'),
+        },
+        partials: {
+            partial: 'project',
+        },
+    })
+})
+
+app.get('/signup', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: getTitle('signup'),
+            script: getScript('signup'),
+        },
+        partials: {
+            partial: 'signup',
+        },
+    })
+})
 
 //listening port
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+    console.log(`Server listening on port ${PORT}`)
+})
