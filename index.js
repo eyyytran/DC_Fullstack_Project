2066df4d602b972e524e26ba0aba24ed19f49a70
Jason <jason.blunck@gmail.com>
2022-08-04 14:20:28 -0500

    required dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require("dotenv").config();
 const cors = require('cors')
 const es6Renderer = require('express-es6-template-engine')
 const checkLogin = require('./util/checkLogin')
